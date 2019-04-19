cfb8e3d4336de6ecc768385fe80a466ae12aa84c
dh <2964727542@qq.con>
2019-04-19 20:27:44 +0800

    图书-英雄项目的后台管理

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import BookInfo,HeroInfo
+
 
 # Register your models here.
+class HeroInfoInline(admin.StackedInline):
+    model = HeroInfo
+    extra = 1
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['btitle']
+    search_fields = ['btitle']
+    list_per_page = 5
+    inlines = [HeroInfoInline]
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'book']
+    list_filter = ['hgender', 'hbook']
+    search_fields = ['hname', 'hgender', 'hbook']
+    list_per_page = 10
+
+
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo, HeroInfoAdmin)
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.btitle
+
+    def name(self):
+        return self.btitle
+    name.short_description = '书籍名称'
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.CharField(max_length=10)
+    hconent = models.CharField(max_length=50)
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hname
+
+    def name(self):
+        return self.hname
+    name.short_description = '人物'
+
+    def book(self):
+        return self.hbook
+    book.short_description = '所属书籍'
