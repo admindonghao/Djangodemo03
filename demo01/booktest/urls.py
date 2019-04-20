11b934ec8e6c1831e7239c5d30b279d5770300b6
dh <2964727542@qq.con>
2019-04-20 10:30:58 +0800

    图书-英雄项目的增删改查功能

--- demo01/booktest/urls.py
+++ demo01/booktest/urls.py
@@ -7,5 +7,13 @@ app_name = 'booktest'
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^list/$', list, name='list'),
-    url(r'^detail/(\d+)/$', detail, name='detail')
+    url(r'^detail/(\d+)/$', detail, name='detail'),
+    url(r'^deletebook/(\d+)$', deletebook, name='deletebook'),
+    url(r'^deletehero/(\d+)$', deletehero, name='deletehero'),
+    url(r'^addbook/$', addbook, name='addbook'),
+    url(r'^addbooks/$', addbooks, name='addbooks'),
+    url(r'^update/(\d+)/$', update, name='update'),
+    url(r'^updates/(\d+)/$', updates, name='updates'),
+    url(r'^create/(\d+)/$', create, name='create'),
+    url(r'^creates/(\d+)/$', creates, name='creates'),
 ]
--- demo01/booktest/views.py
+++ demo01/booktest/views.py
@@ -4,15 +4,74 @@ from django.http import HttpResponse,HttpResponseRedirect
 
 
 # Create your views here.
+
+
+# 首页视图函数
 def index(requset):
     return render(requset, 'booktest/index.html', {})
 
 
+# 列表页视图函数
 def list(requset):
     books = BookInfo.objects.all()
     return render(requset, 'booktest/list.html', {'booklist':books})
 
 
+# 详情页视图函数
 def detail(requset, id):
     book = BookInfo.objects.get(pk = id)
     return render(requset, 'booktest/detail.html', {'book':book})
+
+
+# 删除图书视图函数
+def deletebook(requset, id):
+    BookInfo.objects.get(pk=id).delete()
+    return HttpResponseRedirect('/booktest/list/')
+
+
+
+def deletehero(requset, id):
+    HeroInfo.objects.get(pk=id).delete()
+    return HttpResponseRedirect('/booktest/list/')
+
+
+# 添加的视图函数
+def addbook(requset):
+    return render(requset, 'booktest/addbook.html', {})
+
+
+def addbooks(requset):
+    b = BookInfo()
+    b.btitle = requset.POST['btitle']
+    b.save()
+    return HttpResponseRedirect('/booktest/list/')
+
+
+# 更新英雄信息
+def update(requset, id):
+    hero = HeroInfo.objects.get(pk=id)
+    return render(requset, 'booktest/update.html', {'hero':hero})
+
+
+def updates(requset, id):
+    h = HeroInfo.objects.get(pk=id)
+    h.hname = requset.POST['name']
+    h.hgender = requset.POST['gender']
+    h.hconent = requset.POST['conent']
+    h.save()
+    return HttpResponseRedirect('/booktest/list/')
+
+
+def create(requset, id):
+    book = BookInfo.objects.get(pk=id)
+    return render(requset, 'booktest/create.html', {'book':book})
+
+
+def creates(requset,id):
+    h = HeroInfo()
+    h.hname = requset.POST['name']
+    h.hgender = requset.POST['gender']
+    h.hconent = requset.POST['conent']
+    h.hbook = BookInfo.objects.get(pk=id)
+    h.save()
+    return HttpResponseRedirect('/booktest/list/')
