fef5dd00fcc12e2f802733abff495fab48b81ddb
dh <2964727542@qq.con>
2019-04-20 14:36:12 +0800

    投票系统的增删改查功能

--- demo02/polls/urls.py
+++ demo02/polls/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
     url(r'^list/$', list, name='list'),
     url(r'^detail/(\d+)/$', detail, name='detail'),
     url(r'^details/(\d+)/$', details, name='details'),
+    url(r'^deleteiss/(\d+)/$', deleteiss, name='deleteiss'),
+    url(r'^deletec/(\d+)/$', deletec, name='deletec'),
+    url(r'^updeta/(\d+)/$', update, name='update'),
+    url(r'^updetas/(\d+)/$', updates, name='updates'),
+    url(r'^addw/$', addw, name='addw'),
+    url(r'^addws/$', addws, name='addws'),
+    url(r'^createch/(\d+)/$', createch, name='createch'),
+    url(r'^createchs/(\d+)/$', createchs, name='createchs'),
+    url(r'^result/$', result, name='result'),
 ]
--- demo02/polls/views.py
+++ demo02/polls/views.py
@@ -10,7 +10,7 @@ def index(requset):
 
 def list(requset):
     pl = IssueList.objects.all()
-    print(pl)
+    # print(pl)
     return render(requset, 'polls/list.html', {'polls':pl})
 
 
@@ -26,3 +26,57 @@ def details(request, id):
     cho.number += 1
     cho.save()
     return render(request, 'polls/details.html', {'poll':poll})
+
+
+def deleteiss(request, id):
+    IssueList.objects.get(pk=id).delete()
+    return HttpResponseRedirect('/polls/list/')
+
+
+def deletec(request, id):
+    c = ChioceList.objects.get(pk=id)
+    c.delete()
+    # print(c.is_issue.id)
+    return HttpResponseRedirect('/polls/detail/{}/'.format(c.is_issue.id))
+
+
+def update(requset, id):
+    iss = IssueList.objects.get(pk=id)
+    return render(requset, 'polls/update.html', {'iss':iss})
+
+
+def updates(requset, id):
+    i = IssueList.objects.get(pk=id)
+    i.issue = requset.POST['issue']
+    i.save()
+    return HttpResponseRedirect('/polls/list/')
+
+
+def addw(requset):
+    return render(requset, 'polls/addw.html', {})
+
+
+def addws(requset):
+    i = IssueList()
+    i.issue = requset.POST['wen']
+    i.save()
+    return HttpResponseRedirect('/polls/list/')
+
+
+def createch(requset, id):
+    iss = IssueList.objects.get(pk=id)
+    return render(requset, 'polls/create.html', {'iss':iss})
+
+
+def createchs(requset,id):
+    i = IssueList.objects.get(pk=id)
+    c = ChioceList()
+    c.options = requset.POST['option']
+    c.is_issue = i
+    c.save()
+    return HttpResponseRedirect('/polls/detail/{}/'.format(id))
+
+
+def result(request):
+    cl = ChioceList.objects.all()
+    return render(request, 'polls/result.html', {'cl':cl})
