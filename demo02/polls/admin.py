4e6f17effd7296e3691febf4c211d9a3467b7bec
dh <2964727542@qq.con>
2019-04-20 11:14:48 +0800

    投票系统的后台管理

--- demo02/polls/admin.py
+++ demo02/polls/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import IssueList,ChioceList
+
 
 # Register your models here.
+class ChioceListInlines(admin.StackedInline):
+    model = ChioceList
+    extra = 2
+
+
+class IssueListAdmin(admin.ModelAdmin):
+    list_display = ['iss',]
+    search_fields = ['issue']
+    inlines = [ChioceListInlines]
+
+
+class ChioceListAdmin(admin.ModelAdmin):
+    list_display = ['isss', 'opt']
+    list_filter = ['is_issue']
+    search_fields = ['is_issue']
+    list_per_page = 10
+
+
+admin.site.register(IssueList, IssueListAdmin)
+admin.site.register(ChioceList, ChioceListAdmin)
--- demo02/polls/models.py
+++ demo02/polls/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class IssueList(models.Model):
+    issue = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.issue
+
+    def iss(self):
+        return self.issue
+
+    iss.short_description = '问题'
+
+
+class ChioceList(models.Model):
+    options = models.CharField(max_length=20)
+    number = models.IntegerField(default=0)
+    is_issue = models.ForeignKey('IssueList', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.options
+
+    def opt(self):
+        return self.options
+    opt.short_description = '选项'
+
+    def isss(self):
+        return self.is_issue
+    isss.short_description = '问题'
